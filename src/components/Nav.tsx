e446d48ee0f390ee799f37d682fe6410db15eefa
Tharinda P Anurajeewa <tharindahp@gmail.com>
2021-06-21 22:31:27 +0530

    created responsive nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,23 +1,80 @@
-import React, { Component } from "react";
-
 import "../styles/Nav.css";
 
 import NetflixLogo from "../assets/netflix-logo.png";
 import SearchIcon from "../assets/search-white.svg";
 import MobileMenu from "../assets/menu-white.svg";
+import CloseIcon from "../assets/close_white_24dp.svg";
+
+import { useState } from "react";
+import { useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
 
 function Nav() {
+  const [blackNav, setBlackNav] = useState(false);
+  const [mobileMenuClick, setMobileMenuClick] = useState(false);
+  const isMobile = useMediaQuery({ query: "(min-width: 600px)" });
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNav);
+    window.addEventListener("resize", checkMobile);
+  }, []);
+
+  const transitionNav = () => {
+    if (window.scrollY > 100) setBlackNav(true);
+    else setBlackNav(false);
+  };
+
+  const checkMobile = () => {
+    if (isMobile) setMobileMenuClick(true);
+    else setMobileMenuClick(false);
+  };
+
+  const clickedMobileMenu = () => {
+    setMobileMenuClick(true);
+  };
+
+  const clickedMobileMenuClose = () => {
+    setMobileMenuClick(false);
+  };
+
+  const showMobileIcon = () => {
+    if (!mobileMenuClick)
+      return (
+        <img
+          onClick={clickedMobileMenu}
+          className="nav__mobile-menu"
+          src={MobileMenu}
+          alt="Mobile menu"
+        />
+      );
+    else
+      return (
+        <img
+          onClick={clickedMobileMenuClose}
+          className="nav__mobile-menu-close"
+          src={CloseIcon}
+          alt="Mobile menu close"
+        />
+      );
+  };
+
   return (
-    <div className="nav nav-black">
+    <div className={`nav ${blackNav && "nav-black"}`}>
       <img className="nav__logo" src={NetflixLogo} alt="Netflix logo" />
 
-      <div className="nav__links">
+      <div className={mobileMenuClick ? "nav__mobile-links" : "nav__links"}>
         <ul>
           <li>
             <a href="#">TV Shows</a>
           </li>
           <li>
             <a href="#">Movies</a>
+          </li>
+          <li>
+            <a href="#">Recently Added</a>
+          </li>
+          <li>
+            <a href="#">My List</a>
           </li>
         </ul>
       </div>
@@ -30,9 +87,7 @@ function Nav() {
         alt="Avatar"
       />
 
-      <div className="nav__mobile">
-        <img className="nav__mobile-menu" src={MobileMenu} alt="Mobile menu" />
-      </div>
+      <div className="nav__mobile">{showMobileIcon()}</div>
     </div>
   );
 }
