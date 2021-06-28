bab6476791c0563660aad3d23701656bad0c5ee7
Tharinda P Anurajeewa <tharindahp@gmail.com>
2021-06-28 22:16:03 +0530

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import React from "react";
+//import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import HomeScreen from "./screens/HomeScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
 
 import "./styles/root.css";
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+      <ProfileScreen />
+      {/* <WelcomeScreen /> */}
+      {/* <Router>
+        <Switch>
+          <Route path="/">
+            <HomeScreen />
+          </Route>
+          <Route path="/welcome">
+            <WelcomeScreen />
+          </Route>
+        </Switch>
+      </Router> */}
     </div>
   );
 }
